80dc2a426690bfd2546625d75bd895b54435da0a
Nathan Wittstock <nate@fardogllc.com>
2013-05-04 15:43:37 -0700

    Makes jinja work on subdirectories.

--- jinja.py
+++ jinja.py
@@ -12,12 +12,22 @@ os.chdir("source/")
 class JinjaEventHandler(PatternMatchingEventHandler):
 
     def on_modified(self, event):
+        # Open our input file
         template_file = codecs.open(event.src_path, 'r')
         template = Template(template_file.read())
 
+        common_prefix = os.path.commonprefix([
+            os.getcwd(),
+            event.src_path,
+            ])
+        output_directory = "../app%s" % os.path.dirname(event.src_path).replace(common_prefix, "")
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+        # Open our output file
         output_file_name = os.path.basename(event.src_path).replace(".tpl", "")
         output_file = \
-                codecs.open("../app/%s" % output_file_name, 'w')
+                codecs.open("%s/%s" % (output_directory, output_file_name), 'w')
 
         output_file.write(template.render(jinjaconfig=jinjaconfig))
         print "jinja wrote file %s" % output_file_name
