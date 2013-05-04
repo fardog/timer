1d3f09f26d6a4cd07bcf693043886120e70a376c
Nathan Wittstock <nate@fardogllc.com>
2013-05-04 16:19:21 -0700

    Fix cname

--- jinjaconfig.py
+++ jinjaconfig.py
@@ -2,9 +2,11 @@ css = [
         "stylesheets/normalize.css",
         "stylesheets/app.css",
         ]
+
 javascripts_header = [
         "javascripts/vendor/custom.modernizr.js",
         ]
+
 javascripts_footer = [
         "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js",
         "javascripts/foundation/foundation.js",
