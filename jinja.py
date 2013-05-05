b5502fde61a3a8314e7292b4f6c7493b4e2b786f
Nathan Wittstock <nate@fardogllc.com>
2013-05-04 22:24:38 -0700

    Move form and use WAV audio.

--- jinja.py
+++ jinja.py
@@ -1,5 +1,6 @@
 import time
 import os
+import sys
 import codecs
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
--- jinjaconfig.py
+++ jinjaconfig.py
@@ -10,7 +10,7 @@ javascripts_header = [
 javascripts_footer = [
         "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js",
         "javascripts/foundation/foundation.js",
-        "javascripts/vendor/riffwave.js",
         "javascripts/vendor/juration.js",
+        "javascripts/vendor/jquery.color.js",
         "javascripts/app.js",
         ]
